7210ba3f4a818bf1c7063e01ed441a1381d6b599
Adolfo20bm <abm2491@gmail.com>
2022-12-24 08:56:21 -0500

    finalizacion primera API

--- Dia-3/1-inicios-flask.py
+++ Dia-3/1-inicios-flask.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+#es toda la informacion que puedo leer del usuario, dentro de el body
 from datetime import datetime
 
+usuarios = [
+    {
+        'correo': 'ederiveroman@gmail.com',
+        'nombre': 'eduardo',
+        'apellido': 'de Rivero'
+    }
+]
+
+# __name__ > muestra si el archivo es el archivo principal del proyecto, mostrara el valor de '__main__' y si no entonces mostrara otro valor
 # __name__
-print(__name__)
+#print(__name__)
 app = Flask(__name__)
 
 #usaremos el metodo route
@@ -25,10 +35,40 @@ def estado():
         'hora': hora_servidor
     }
 
-@app.route('/identificarse', methods=['POST'])
-def identificacion():
+@app.route('/registrarse', methods=['POST'])
+def registro():
+    #request.data devuelve el body en binary(formato bytes)
+    print(request.data)
+    # request.get_json() - convierte la informacion entrante en un dicionario para que pueda ser utilizada sin problemas en python
+    print(request.get_json())
+    body = request.get_json()
+    # iterar el arreglo de usuarios y validar que no existe un usuariocon ese  correo provenieitnte del body
+    for usuario in usuarios:
+        print(usuario)
+        correo = usuario.get('correo')
+        if correo == body.get('correo'):
+            return{
+                'message': 'El usuario ya esta registrado'
+            }
+    
+    #como extraer la informacion de un diccionario
+    #body.get('correo')
+    #body['correo']
+    
+    usuarios.append(body)
+    # si no existe entonces agregar ese correo al arreglo caso contrario retornar un mensaje que diga que el uusaio ya esta regiostrado
     return {
-        'message': 'Identificacion registrada exitosamente'
+        'message': 'Usuario registrado exitosamente'
+    }
+
+#crear un endpoint que sea /listar-usuarios y este devolbvera el siguiente resultado
+# { message: 'Los usuarios son', content: [{},{}]}
+@app.route('/listar-usuarios', methods=['GET'])
+def listar():
+    return {
+        'message':'Los usuarios son',
+        'content':usuarios
+
     }
 
 
@@ -38,3 +78,4 @@ def identificacion():
 #debug indicara si guardamos algun archivo dentro
 # del proyecto reiniciara automaticamente el servidor
 app.run(debug=True)
+
