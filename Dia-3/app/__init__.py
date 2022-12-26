e413ffd3593b4fb87accce2bbe73810776665b5a
Adolfo20bm <abm2491@gmail.com>
2022-12-25 19:40:36 -0500

    estructura basica de mi proyectoen kflask

--- Dia-3/app/__init__.py
+++ Dia-3/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,4 +18,22 @@ usuarios = [
 
 @app.route("/<dni>")
 def home(dni):
-    return f'Hola tu numero de DNI es {dni}'
+    for usuario in usuarios:
+        if usuario['dni']==dni:
+            return f'El nombre del usuario es: {usuario["nombre"]}'
+    return f'El suuario con el dni: {dni} no fue encontrado'
+
+@app.route("/auth/login", methods=['POST'])
+def login():
+    usuario = {
+        "username": "paolo",
+        "password": "osito123"
+    }
+    json = request.get_json()
+    if (usuario['username']==json['username'] and usuario['password']==json['password']):
+        return 'Usuario logeado'
+    return 'Las credenciales son incorrectas'
+
+@app.route("/products", methods=['POST','GET','PUT','DELETE'])
+def products():
+    return 'Esta es la ruta productos'
