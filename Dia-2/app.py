f3629910b6045481b68ceba1306fdb32c3c73c19
Adolfo20bm <abm2491@gmail.com>
2022-12-26 14:10:27 -0500

    avance formulario jinja

--- Dia-2/app.py
+++ Dia-2/app.py
@@ -60,6 +60,36 @@ def devolver_alumnos():
 
     return render_template('mostrar_alumnos.html', alumnos=resultado_final, mensaje='Hola desde Falask')
 
+@app.route('/agregar-alumno', methods=['GET'])
+def agregar_alumno():
+    #crea conexion
+    cursor = mysql.connection.cursor()
+    #clausuala a determinada tabla
+    cursor.execute("SELECT * FROM alumnos")
+    #devolver toda la info de esa consulta
+    resultado = cursor.fetchall()
+    #print(resultado)
+    resultado_final = []
+    for alumno in resultado:
+        alumno_diccionario = {
+            'id': alumno[0],
+            'nombre': alumno[1],
+            'ape_paterno': alumno[2],
+            'ape_materno': alumno[3],
+            'correo': alumno[4],
+            'num_emergencia': alumno[5],
+            'curso_id': alumno[6]
+        }
+        print(alumno_diccionario)
+        resultado_final.append(alumno_diccionario)
+
+#    return {
+#        'message': 'Los alumnos son:',
+#        'content': resultado_final
+#    }
+
+    return render_template('agregar_alumno.html', alumnos=resultado_final, mensaje='Hola desde Mostrar alumnos')
+
 
 
 
